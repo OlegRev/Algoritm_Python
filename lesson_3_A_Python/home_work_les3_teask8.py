a0f47fb1a1871d836e11db2091b40612603c13b8
OlegRev <olegglukharev92@gmailcom>
2019-06-04 15:29:06 +0300

    'исправил неточность заполнение матрицы 5х4 идет 4 стобца матрицы заполняются с ввода пользователя 5 столбец заполняется случайными числами от 0 до 99 после каждой строки матрицы выодиться сумма строки'

--- lesson_3_A_Python/home_work_les3_teask8.py
+++ lesson_3_A_Python/home_work_les3_teask8.py
@@ -3,15 +3,14 @@
 # Программа должна вычислять сумму введенных элементов
 # каждой строки и записывать ее в последнюю ячейку строки.
 # В конце следует вывести полученную матрицу.
-
-matrix = [[int(input('Введите натуральные числа')) for _ in range(5)] for _ in range(4)]
+import random
+matrix = [[int(input('Введите натуральные числа')) for _ in range(4)] for _ in range(4)]
 
 for line in matrix:
+    line += [random.randint(0,99)]
     sum_line = 0
     for i, item in enumerate(line):
         sum_line += item
-
         print(f'{item:>5}', end='')
 
     print(f'   |сумма элементов строки = {sum_line}')
-
