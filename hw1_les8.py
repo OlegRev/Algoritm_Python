614ea8dc45800f4f004c6e418a55f4044ddeeb92
OlegRev <olegglukharev92@gmail.com>
2019-07-07 20:53:20 +0300

    "сделал замену подстрок в формате букв на хеш подстрок"

--- hw1_les8.py
+++ hw1_les8.py
@@ -7,18 +7,17 @@
 #func("sova")
 #9
 #
+import hashlib
 def num_substr(string):
     """функция получения количества подстрок в строке"""
     string = str(string)
     lenght = len(string)
     substrings = []
-    [[substrings.append(string[i:i+n+1]) for i in range(lenght-n) if string[i:i+n+1] not in substrings]
-     for n in range(lenght-1)]
-    print(substrings)
+    [[substrings.append(hashlib.sha1(string[i:i+n+1].encode('utf-8')).hexdigest()) for i in range(lenght-n)
+      if hashlib.sha1(string[i:i+n+1].encode('utf-8')).hexdigest() not in substrings]for n in range(lenght-1)]
     return len(substrings)
 
-print(num_substr('knsdfklklsdf'))
-
+print(num_substr('kate'))
 def test_func(func):
     print("Тестируем: ", func.__doc__)
     print("testcase #1: ", end="")
@@ -37,6 +36,13 @@ def test_func(func):
     string = 'eggs'
     num = 8
     print("Ok" if num_substr(string) == num else "Fail")
+
+    print("Тестируем: ", func.__doc__)
+    print("testcase #4: ", end="")
+    string = 'Papa'
+    num = 8
+    print("Ok" if num_substr(string) == num else "Fail")
+
 
 
 if __name__ == "__main__":
